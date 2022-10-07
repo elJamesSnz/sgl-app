b3a2f291d2fc0d271c4c2a824df6cf040c1f8077
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2022-10-07 10:08:20 -0500

    Page Equipamiento
    Page temporal para visualizar formulario de Equipamiento

---
+++ pages/Equipamiento.js
@@ -0,0 +1,11 @@
+import MainLayout from "../layouts/MainLayout";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <MainLayout className="home">
+      aaaaaaaaaaaaaaaa
+      </MainLayout>
+    </div>
+  );
+}
