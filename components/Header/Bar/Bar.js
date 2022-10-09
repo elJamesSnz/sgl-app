8a7cae550c0ba9fe282e6718e8beeb5e12d9d910
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2022-10-08 23:23:38 -0500

    Classes Bar
    Clases par estilar la Barra principal

--- components/Header/Bar/Bar.js
+++ components/Header/Bar/Bar.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Bar() {
   return (
     <div className="top-bar">
-      <p>Bar</p>
+      <a className="SGL">LOGO SGL</a>
+      <a className="TITLE">Unidad Profesional Interdisciplinaria en Ingeniería y
+Tecnologías Avanzadas</a>
+      <a className="IPN">LOGO IPN</a>
     </div>
   );
 }
