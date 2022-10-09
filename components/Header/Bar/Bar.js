f69a08cd1d422565c040173dd3ce18db200ed78c
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2022-10-09 01:27:06 -0500

    IPN Logo
    Ajuste del logotipo del IPN

--- components/Header/Bar/Bar.js
+++ components/Header/Bar/Bar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from 'next/image'
+import mypic from '../../../resources/ipnWhite.png'
 
 export default function Bar() {
   return (
@@ -6,7 +8,19 @@ export default function Bar() {
       <a className="SGL">LOGO SGL</a>
       <a className="TITLE">Unidad Profesional Interdisciplinaria en Ingeniería y
 Tecnologías Avanzadas</a>
-      <a className="IPN">LOGO IPN</a>
+      <LogoIPN/>
     </div>
+  );
+}
+
+function LogoIPN(){
+  return(
+    <Image
+    className="IPN"
+    src={mypic}
+    alt="Picture of the author"
+    width="50px"
+    height="70px"
+    />
   );
 }
