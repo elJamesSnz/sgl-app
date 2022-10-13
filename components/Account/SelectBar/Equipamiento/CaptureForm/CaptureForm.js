48e1be5bc5e817bace3fdb0f27fee5d0be48a878
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2022-10-13 02:11:22 -0500

    Equipamiento Form
    Estructura JSON del formulario Equipamiento

--- components/Account/SelectBar/Equipamiento/CaptureForm/CaptureForm.js
+++ components/Account/SelectBar/Equipamiento/CaptureForm/CaptureForm.js
@@ -29,7 +29,7 @@ export default function CaptureForm(props) {
 }
 
 function FormCapturaEquipos(props) {
-  const { onShowForm } = props;
+  const { onShowForm,values } = props;
   //Validador para ocultar Forms
   const [componentDisabled, setComponentDisabled] = useState(false);
   const onFormLayoutChange = ({ disabled }) => {
@@ -66,54 +66,52 @@ function FormCapturaEquipos(props) {
           onChange={formik.handleChange}
           error={formik.errors.nombre}
       />
-
-      <Item>Descripción:</Item> 
-          <TextArea 
-            name="descripcion"
-            type="text"
-            rows={4}
-            placeholder="Breve descripcion del la utilidad o propósito principal del equipo"
-            onChange={formik.handleChange}
-            error={formik.errors.descripcion}
-          />    
-
-        <Item>Código de barras:</Item>
-          <Input 
-          name="codigo"
-          type="text"
-          placeholder="Código de barras" 
-          onChange={formik.handleChange}
-          error={formik.errors.codigo}
-          />
         
-        <Item>Modelo:</Item>
-          <Input 
+      <Item>Modelo:</Item>
+        <Input 
           name="modelo"
           type="text"
           placeholder="Modelo" 
           onChange={formik.handleChange}
           error={formik.errors.modelo}
-          />
+        />
         
-        <Item>Año:</Item>  
-          <Input 
+      <Item>Año:</Item>  
+        <Input 
           name="ano"
           type="text"
           placeholder="Año" 
           onChange={formik.handleChange}
           error={formik.errors.ano}
-          />
+        />
 
-        <br></br> 
-        <Checkbox
+      <Item>Código de barras:</Item>
+        <Input 
+          name="codigo"
+          type="text"
+          placeholder="Código de barras" 
+          onChange={formik.handleChange}
+          error={formik.errors.codigo}
+        />
+      
+      
+      <Item>Partida:</Item>  
+          <Input //Se manda el id_carrera
+          name="partida"
+          type="text"
+          placeholder="Numero de Partida del equipo" 
+          onChange={formik.handleChange}
+          error={formik.errors.partida}
+          /> 
+
+      <Checkbox
           checked={componentDisabled}
           onChange={(e) => setComponentDisabled(e.target.checked)}
-          
-        >
+         >
           No cuenta con manual de usuario
-        </Checkbox>
+      </Checkbox>
 
-          <Input 
+        <Input 
           name="manual"
           type="text"
           placeholder="Nombre del manual de usuairo" 
@@ -121,24 +119,104 @@ function FormCapturaEquipos(props) {
           disabled={componentDisabled}
           onChange={formik.handleChange}
           error={formik.errors.manual}
-          />
+        />
         
         <Item>Estado:</Item> 
         <Select
           //name="estado"
           //type="text"
-          defaultValue="Funcional"
+          defaultValue="1"
           style={{
             width: 170,
           }}
           onChange={handleChange}
           //error={formik.errors.estado}
         >
-          <Option value="Funcional" >Funcional</Option>
-          <Option value="No Funcional">No Funcional</Option>
-          <Option value="Funcional con Fallo">Funcional con Fallo</Option>
+          <Option value="1" >Funcional</Option>
+          <Option value="2">No Funcional</Option>
+          <Option value="3">Funcional con Fallo</Option>
         </Select>
-              
+
+        <Item>Descripción del fallo:</Item>  
+          <TextArea 
+          name="fallo"
+          type="text"
+          rows={4}
+          placeholder="Descripción del fallo presentado" 
+          onChange={formik.handleChange}
+          error={formik.errors.fallo}
+        />
+          
+        <Item>Disponibilidad:</Item> 
+        <Select
+          //name="estado"
+          //type="text"
+          defaultValue="1"
+          style={{
+            width: 170,
+          }}
+          onChange={handleChange}
+          //error={formik.errors.estado}
+        >
+          <Option value="1" >Disponible</Option>
+          <Option value="2">No disponible</Option>
+        </Select>
+
+        <Item className="descripcion_item">DESCRIPCION:</Item> 
+        <Form className="descripcion">
+        <Item>Utilidad:</Item>  
+          <Input 
+          name="utilidad"
+          type="text"
+          placeholder="Utilidad" 
+          onChange={formik.handleChange}
+          error={formik.errors.utilidad}
+          />
+        <Item>Carrera:</Item>  
+          <Select
+            id="carrera"
+            name="carrera"
+            type="text" 
+            style={{
+              width: 170,
+            }}
+            onChange={handleChange}
+            //onChange={formik.handleChange}
+            //error={formik.errors.carrera}
+          >
+            
+            <Option value="1">Telematica</Option>
+            <Option value="2">Mecatronica</Option>
+            <Option value="3">Bionica</Option>
+          </Select>
+
+        <Item>Asignatura:</Item>  
+          <Input //Se manda el id_carrera
+          name="asignatura"
+          type="text"
+          placeholder="Asignatura" 
+          onChange={formik.handleChange}
+          error={formik.errors.asignatura}
+          />  
+          <Item>Practicas:</Item> 
+          <TextArea 
+            name="practicas"
+            type="text"
+            rows={4}
+            placeholder="Practicas en las que se utiliza el equipo"
+            onChange={formik.handleChange}
+            error={formik.errors.practicas}
+          /> 
+          <Item>Numero Alumnos:</Item>  
+          <Input //Se manda el id_carrera
+          name="numAlumnos"
+          type="text"
+          placeholder="Numero de alumnos beneficiados" 
+          onChange={formik.handleChange}
+          error={formik.errors.numAlumnos}
+          />   
+        </Form>
+
         <Item valuePropName="fileList">Evidencia:</Item>
           <Upload action="/upload.do" listType="picture-card">
             <div>
@@ -163,23 +241,50 @@ function FormCapturaEquipos(props) {
 function initualValues() {
   return {
     nombre: "",
-    descripcion: "",
     codigo: "",
     modelo:"",
     ano:"",
-    manual:"",
+    fallo:"",
     estado:"",
+    manual:"",
+    laboratorio:"",
+
+    disponibilidad:"",
+    //Descripcion
+    utilidad: "",
+    carrera: "",
+    asignatura:"",
+    practicas:"",
+    
+    partida:"",
+    
+    
   };
 }
 
 function validationSchema() {
+
   return {
-    nombre: Yup.string(),
-    descripcion: Yup.string(),
-    codigo: Yup.string(),
+    nombre:Yup.string(),//.required(true),
+    codigo:Yup.string(),//.required("true"),
     modelo:Yup.string(),
     ano:Yup.string(),
-    manual:Yup.string(),
+    fallo:Yup.string(),
     estado:Yup.string(),
+    manual:Yup.string(),
+    laboratorio:Yup.string(),
+
+    disponibilidad:Yup.string(),
+    //Descripcion
+    utilidad:Yup.string(),
+    carrera:Yup.string(),
+    asignatura:Yup.string(),
+    practicas:Yup.string(),
+    numAlumnos:Yup.string(),
+    
+    partida:Yup.string(),
+    
   };
 }
+
+
