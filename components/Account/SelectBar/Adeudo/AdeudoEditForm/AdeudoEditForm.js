b5f1422f693003d1c733644d1ece77d47a2dba5c
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2022-10-18 08:49:22 -0500

    Editable
    Added editable button

--- components/Account/SelectBar/Adeudo/AdeudoEditForm/AdeudoEditForm.js
+++ components/Account/SelectBar/Adeudo/AdeudoEditForm/AdeudoEditForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Icon, Input, Form,Item } from "semantic-ui-react";
+import { Button, Icon, Input, Form, Item } from "semantic-ui-react";
 
 export default function AdeudoEditForm(props) {
   const { viewAdeudo } = props;
@@ -15,83 +15,93 @@ export default function AdeudoEditForm(props) {
           <p>{viewAdeudo.idlaboratorio}</p>
         </div> */}
         <Form
-        className="adeudo_edit"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-        style={{ padding: "10px" }}
+          className="adeudo_edit"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+          style={{ padding: "10px" }}
         >
-            <Item>Laboratorio</Item>
-            <Input className={`nombrelaboratorio ${editable ? "" : "disabled"}`}
+          <Item>Laboratorio</Item>
+          <Input
+            className={`nombrelaboratorio ${editable ? "" : "disabled"}`}
             name="nombrelaboratorio"
             type="text"
             placeholder="Nombre del Laboratorio"
-            value ={`${viewAdeudo.nombrelaboratorio}`}
-            />
-            <Item>Nombre del Alumno:</Item>
-            <Input className={`nombrealumno ${editable ? "" : "disabled"}`}
+            value={`${viewAdeudo.nombrelaboratorio}`}
+          />
+          <Item>Nombre del Alumno:</Item>
+          <Input
+            className={`nombrealumno ${editable ? "" : "disabled"}`}
             name="nombre"
             type="text"
             placeholder="Nombre del Alumno"
-            value ={`${viewAdeudo.nombrealumno}`}
-            />
-            <Item>Boleta:</Item>
-            <Input className={`boleta ${editable ? "" : "disabled"}`}
+            value={`${viewAdeudo.nombrealumno}`}
+          />
+          <Item>Boleta:</Item>
+          <Input
+            className={`boleta ${editable ? "" : "disabled"}`}
             name="boleta"
             type="text"
             rows={4}
             placeholder="Boleta del alumno"
-            value ={`${viewAdeudo.boleta}`}
-            />
-            <Item>Carrera:</Item>
-            <Input className={`carrera ${editable ? "" : "disabled"}`}
+            value={`${viewAdeudo.boleta}`}
+          />
+          <Item>Carrera:</Item>
+          <Input
+            className={`carrera ${editable ? "" : "disabled"}`}
             name="carrera"
             type="text"
             placeholder="Carrera"
-            value ={`${viewAdeudo.carrera}`}
-            />
-            <Item>Correo:</Item>
-            <Input className={`correo ${editable ? "" : "disabled"}`}
+            value={`${viewAdeudo.carrera}`}
+          />
+          <Item>Correo:</Item>
+          <Input
+            className={`correo ${editable ? "" : "disabled"}`}
             name="correo"
             type="text"
             placeholder="Correo"
-            value ={`${viewAdeudo.correo}`}
-            />   
-            <Item>Fecha de peticion</Item>
-            <Input className={`fecha_peticion ${editable ? "" : "disabled"}`}
-                name="fecha_peticion"
-                type="text"
-                placeholder="MM/DD/AAAA"
-                value ={`${viewAdeudo.fecha_peticion}`}
-            />
-            <Item>Fecha de entrega:</Item>
-            <Input className={`fecha_entrega ${editable ? "" : "disabled"}`}
-                name="fecha_entrega"
-                type="text"
-                placeholder="MM/DD/AAAA"
-                value ={`${viewAdeudo.fecha_entrega}`}
-            />
-            <Item>Asignatura:</Item>
-            <Input className={`materia ${editable ? "" : "disabled"}`}
-                name="materia"
-                type="text"
-                placeholder="Asignatura"
-                value ={`${viewAdeudo.materia}`}
-            />
-            <Item>Profesor:</Item>
-            <Input className={`profesor ${editable ? "" : "disabled"}`}
-                name="profesor"
-                type="text"
-                placeholder="Profesor"
-                value ={`${viewAdeudo.profesor}`}
-            />
-            <Item>Estatus:</Item>
-            <Input className={`estatus ${editable ? "" : "disabled"}`}
-                name="estatus"
-                type="text"
-                placeholder="Estatus"
-                value ={`${viewAdeudo.estatus ? "Entregado":"No entregado"}`}
-            />
+            value={`${viewAdeudo.correo}`}
+          />
+          <Item>Fecha de peticion</Item>
+          <Input
+            className={`fecha_peticion ${editable ? "" : "disabled"}`}
+            name="fecha_peticion"
+            type="text"
+            placeholder="MM/DD/AAAA"
+            value={`${viewAdeudo.fecha_peticion}`}
+          />
+          <Item>Fecha de entrega:</Item>
+          <Input
+            className={`fecha_entrega ${editable ? "" : "disabled"}`}
+            name="fecha_entrega"
+            type="text"
+            placeholder="MM/DD/AAAA"
+            value={`${viewAdeudo.fecha_entrega}`}
+          />
+          <Item>Asignatura:</Item>
+          <Input
+            className={`materia ${editable ? "" : "disabled"}`}
+            name="materia"
+            type="text"
+            placeholder="Asignatura"
+            value={`${viewAdeudo.materia}`}
+          />
+          <Item>Profesor:</Item>
+          <Input
+            className={`profesor ${editable ? "" : "disabled"}`}
+            name="profesor"
+            type="text"
+            placeholder="Profesor"
+            value={`${viewAdeudo.profesor}`}
+          />
+          <Item>Estatus:</Item>
+          <Input
+            className={`estatus ${editable ? "" : "disabled"}`}
+            name="estatus"
+            type="text"
+            placeholder="Estatus"
+            value={`${viewAdeudo.estatus ? "Entregado" : "No entregado"}`}
+          />
         </Form>
         <div className="AdeudoEditForm__img">
           <img
@@ -105,10 +115,10 @@ export default function AdeudoEditForm(props) {
         </div>
       </div>
       <div className="AdeudoEditForm__actions">
-        <Button>
+        <Button onClick={() => setEditable(!editable)}>
           <Icon name="edit"></Icon>
         </Button>
-        <Button>
+        <Button disabled={!editable}>
           <Icon name="save"></Icon>
         </Button>
       </div>
--- components/Account/SelectBar/Equipamiento/EquipamientoEditForm/EquipamientoEditForm.js
+++ components/Account/SelectBar/Equipamiento/EquipamientoEditForm/EquipamientoEditForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { Button, Icon, Input, Form,Item } from "semantic-ui-react";
+import { Button, Icon, Input, Form, Item } from "semantic-ui-react";
 
 export default function EquipamientoEditForm(props) {
   const { viewEquip } = props;
-  const [editable] = useState(false);
+  const [editable, setEditable] = useState(false);
   console.log(viewEquip);
   return (
     <div className="EquipamientoEditForm">
@@ -13,83 +13,95 @@ export default function EquipamientoEditForm(props) {
           <p>{viewEquip.idequipo}</p>
         </div> */}
         <Form
-        className="adeudo_edit"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-        style={{ padding: "10px" }}
+          className="adeudo_edit"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+          style={{ padding: "10px" }}
         >
-            <Item>Laboratorio</Item>
-            <Input className={`nombre ${editable ? "" : "disabled"}`}
+          <Item>Laboratorio</Item>
+          <Input
+            className={`nombre ${editable ? "" : "disabled"}`}
             name="nombre"
             type="text"
             placeholder="Nombre del Laboratorio"
-            value ={`${viewEquip.nombre}`}
-            />
-            <Item>Nombre del equipo:</Item>
-            <Input className={`nombreequipo ${editable ? "" : "disabled"}`}
+            value={`${viewEquip.nombre}`}
+          />
+          <Item>Nombre del equipo:</Item>
+          <Input
+            className={`nombreequipo ${editable ? "" : "disabled"}`}
             name="nombreequipo"
             type="text"
             placeholder="Nombre del equipo"
-            value ={`${viewEquip.nombreequipo}`}
-            />
-            <Item>Codigo de Barras:</Item>
-            <Input className={`correo ${editable ? "" : "disabled"}`}
+            value={`${viewEquip.nombreequipo}`}
+          />
+          <Item>Codigo de Barras:</Item>
+          <Input
+            className={`correo ${editable ? "" : "disabled"}`}
             name="codigo_barras"
             type="text"
             placeholder="Codigo de Barras"
-            value ={`${viewEquip.codigo_barras}`}
-            />
-            <Item>Modelo:</Item>
-            <Input className={`modelo ${editable ? "" : "disabled"}`}
+            value={`${viewEquip.codigo_barras}`}
+          />
+          <Item>Modelo:</Item>
+          <Input
+            className={`modelo ${editable ? "" : "disabled"}`}
             name="modelo"
             type="text"
             rows={4}
             placeholder="Modelo"
-            value ={`${viewEquip.modelo}`}
-            />
-            <Item>Año:</Item>
-            <Input className={`ano ${editable ? "" : "disabled"}`}
+            value={`${viewEquip.modelo}`}
+          />
+          <Item>Año:</Item>
+          <Input
+            className={`ano ${editable ? "" : "disabled"}`}
             name="ano"
             type="text"
             placeholder="Año"
-            value ={`${viewEquip.ano}`}
-            /> 
-            <Item>Descripción</Item>
-            <Input className={`Id_descripcion ${editable ? "" : "disabled"}`}
-                name="Id_descripcion"
-                type="text"
-                placeholder="Descripción"
-                value ={`${viewEquip.Id_descripcion}`}
-            />
-            <Item>Nombre del manual:</Item>
-            <Input className={`nombre_manual ${editable ? "" : "disabled"}`}
-                name="nombre_manual"
-                type="text"
-                placeholder="Nombre del manual"
-                value ={`${viewEquip.nombre_manual}`}
-            />
-            <Item>Estatus:</Item>
-            <Input className={`estatus ${editable ? "" : "disabled"}`}
-                name="estatus"
-                type="text"
-                placeholder="Estatus"
-                value ={`${viewEquip.estatus ? "Funcional":"No funcional"}`}
-            />
-            <Item>Descripción del fallo:</Item>
-            <Input className={`fallo ${editable ? "" : "disabled"}`}
-                name="fallo"
-                type="text"
-                placeholder="Descripción del fallo"
-                value ={`${viewEquip.fallo}`}
-            />
-            <Item>Disponibilidad:</Item>
-            <Input className={`Disponibilidad ${editable ? "" : "disabled"}`}
-                name="Disponibilidad"
-                type="text"
-                placeholder="Disponibilidad"
-                value ={`${viewEquip.Disponibilidad ? "Disponible":"No disponible"}`}
-            />
+            value={`${viewEquip.ano}`}
+          />
+          <Item>Descripción</Item>
+          <Input
+            className={`Id_descripcion ${editable ? "" : "disabled"}`}
+            name="Id_descripcion"
+            type="text"
+            placeholder="Descripción"
+            value={`${viewEquip.Id_descripcion}`}
+          />
+          <Item>Nombre del manual:</Item>
+          <Input
+            className={`nombre_manual ${editable ? "" : "disabled"}`}
+            name="nombre_manual"
+            type="text"
+            placeholder="Nombre del manual"
+            value={`${viewEquip.nombre_manual}`}
+          />
+          <Item>Estatus:</Item>
+          <Input
+            className={`estatus ${editable ? "" : "disabled"}`}
+            name="estatus"
+            type="text"
+            placeholder="Estatus"
+            value={`${viewEquip.estatus ? "Funcional" : "No funcional"}`}
+          />
+          <Item>Descripción del fallo:</Item>
+          <Input
+            className={`fallo ${editable ? "" : "disabled"}`}
+            name="fallo"
+            type="text"
+            placeholder="Descripción del fallo"
+            value={`${viewEquip.fallo}`}
+          />
+          <Item>Disponibilidad:</Item>
+          <Input
+            className={`Disponibilidad ${editable ? "" : "disabled"}`}
+            name="Disponibilidad"
+            type="text"
+            placeholder="Disponibilidad"
+            value={`${
+              viewEquip.Disponibilidad ? "Disponible" : "No disponible"
+            }`}
+          />
         </Form>
         <div className="EquipamientoEditForm__img">
           <img
@@ -103,10 +115,10 @@ export default function EquipamientoEditForm(props) {
         </div>
       </div>
       <div className="EquipamientoEditForm__actions">
-        <Button>
+        <Button onClick={() => setEditable(!editable)}>
           <Icon name="edit"></Icon>
         </Button>
-        <Button>
+        <Button disabled={!editable}>
           <Icon name="save"></Icon>
         </Button>
       </div>
